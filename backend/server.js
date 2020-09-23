c41f5f19be09943506f8b0d8c7d37e847bd01583
RhythemParashar <69105370+RhythemParashar@users.noreply.github.com>
2020-09-23 18:29:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+// The server.js file contains the logic of backend. Database connections,  
+// setting up routes and starting the server, all takes place inside server.js
 
-require('dotenv').config();
+
+// Requiring all the dependencies required for setting up backend
+const express = require('express');     // Express is an javascript backend library which works on NodeJS and helps in building the server
+const cors = require('cors');           // Cors is an middleware JS backend library
+const mongoose = require('mongoose');   // Mongoose is an MongoDB connector for NodeJS
+
+require('dotenv').config();             // Requiring the backend variables stored in .env file
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json());                // it will return and accept the data in .json format
 
-const uri = process.env.MONGO_URI;
-mongoose.connect('mongodb://localhost/blog', {
+const uri = process.env.MONGO_URI;      
+mongoose.connect(uri, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
